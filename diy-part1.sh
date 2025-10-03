541db1162320d6127948dcc9693b8d41f7d90c37
DARLING <79352488+shi-an@users.noreply.github.com>
2025-10-04 02:19:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,19 +10,37 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 函数：安全添加 feed 源（如果不存在）
+safe_add_feed() {
+    local feed_name="$1"
+    local feed_url="$2"
+    
+    # 检查是否已存在相同名称的源
+    if ! grep -q "src-git $feed_name " feeds.conf.default; then
+        echo "添加 feed 源: $feed_name"
+        echo "src-git $feed_name $feed_url" >> feeds.conf.default
+    else
+        echo "Feed 源 $feed_name 已存在，跳过添加"
+    fi
+}
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
-#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git innmonkey https://github.com/innmonkey/openwrt-packages' >>feeds.conf.default
-#echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-#echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-#echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-#echo 'src-git 5gsupport https://github.com/Siriling/5G-Modem-Support.git;main' >> feeds.conf.default
-echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
-echo "src-git turboacc https://github.com/chenmozhijin/turboacc" >> feeds.conf.default
-echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+# 取消注释现有的 feed 源（如果需要）
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 安全地添加 feed 源
+safe_add_feed modem "https://github.com/FUjr/modem_feeds.git;main"
+safe_add_feed turboacc "https://github.com/chenmozhijin/turboacc"
+safe_add_feed lienol "https://github.com/Lienol/openwrt-package"
+
+# 其他可选的 feed 源（已注释掉）
+# safe_add_feed helloworld "https://github.com/fw876/helloworld"
+# safe_add_feed passwall "https://github.com/xiaorouji/openwrt-passwall"
+# safe_add_feed kiddin9 "https://github.com/kiddin9/openwrt-packages"
+# safe_add_feed kenzok8 "https://github.com/kenzok8/openwrt-packages"
+# safe_add_feed innmonkey "https://github.com/innmonkey/openwrt-packages"
+# safe_add_feed istore "https://github.com/linkease/istore;main"
+# safe_add_feed nas "https://github.com/linkease/nas-packages.git;master"
+# safe_add_feed nas_luci "https://github.com/linkease/nas-packages-luci.git;main"
+# safe_add_feed 5gsupport "https://github.com/Siriling/5G-Modem-Support.git;main"
+
+echo "Feed 源添加完成"
